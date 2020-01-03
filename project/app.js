743279cef2f90daf39381cfc5381a789a6e93ab7
Rohit Kumar <rohit@Rohits-MacBook-Pro.local>
2020-01-04 00:46:23 +0530

    Port changed to 3000

--- project/app.js
+++ project/app.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 
-server.listen(process.env.PORT || 8080, function() {
+server.listen(process.env.PORT || 3000, function() {
   var host = server.address().address;
   var port = server.address().port;
   //   console.log("Example app listening at http://%s:%s", host, port);
