0f3fa48b447b25ff9875b8a9223ffa5636c22956
Rohit Kumar <rohit@Rohits-MacBook-Pro.local>
2020-01-03 08:19:04 +0530

    Vanilla basic message

--- project/app.js
+++ project/app.js
@@ -1,33 +1,34 @@
+var express = require("express"),
+  redirect = require("express-redirect");
 
-var express = require("express")
-  , redirect = require("express-redirect");
- 
 var app = express();
-redirect(app); 
+redirect(app);
 var router = express.Router();
-var bodyParser = require('body-parser');
-var server = require('http').createServer((request, response) => {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write(`Hello World`);
-	response.end();
+var bodyParser = require("body-parser");
+var server = require("http").createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(`Hello World`);
+  response.end();
 });
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-server.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Example app listening at http://%s:%s', host, port);
+server.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
 });
 
 app.use(router);
-require('./routes/admin/testtxn')(app);
-require('./routes/admin/pgredirect')(app);
-require('./routes/admin/response')(app);
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+require("./routes/admin/testtxn")(app);
+require("./routes/admin/pgredirect")(app);
+require("./routes/admin/response")(app);
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
